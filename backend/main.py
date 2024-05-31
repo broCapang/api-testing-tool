02fa60e2e9b4e8bc210562a383728819ef822ae6
broCapang <U2000429@siswa.um.edu.my>
2024-06-01 01:07:59 +0800

    add new gitignore file, improved the models, and created authentication logic. got .env file too

--- backend/main.py
+++ backend/main.py
@@ -1,12 +1,28 @@
-from fastapi import Depends, FastAPI, HTTPException
+from fastapi import Depends, FastAPI, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from sqlalchemy.orm import Session
+from typing import List
+from datetime import datetime, timedelta, timezone
+from jose import JWTError, jwt
+import os
+from dotenv import load_dotenv
 
 from . import crud, models, schemas
 from .database import SessionLocal, engine
+from typing import Union
+
+# openssl rand -hex 32
+SECRET_KEY = os.getenv("SECRET_KEY")
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
 
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
 
 # Dependency
@@ -17,20 +33,70 @@ def get_db():
     finally:
         db.close()
 
+def create_access_token(data: dict, expires_delta: Union[timedelta, None] = None):
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.now(timezone.utc) + expires_delta
+    else:
+        expire = datetime.now(timezone.utc) + timedelta(minutes=15)
 
-@app.post("/users/", response_model=schemas.User)
-def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
+
+async def get_current_user(token: str = Depends(oauth2_scheme), db: Session = Depends(get_db)):
+    credential_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"}
+    )
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username: str = payload.get("sub")
+        if username is None:
+            raise credential_exception
+        token_data = schemas.TokenData(username=username)
+    except JWTError:
+        raise credential_exception
+    user = crud.get_user_by_username(db, username=token_data.username)
+    if user is None:
+        raise credential_exception
+    return user
+
+async def get_current_active_user(current_user: schemas.User = Depends(get_current_user)):
+    if current_user.disabled:
+        raise HTTPException(status_code=400, detail="Inactive user")
+    return current_user
+
+@app.post("/token", response_model=schemas.Token)
+async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
+    user = crud.authenticate_user(db, form_data.username, form_data.password)
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect username or password",
+            headers={"WWW-Authenticate": "Bearer"}
+        )
+    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token = create_access_token(
+        data={"sub": user.username}, expires_delta=access_token_expires
+    )
+    return {"access_token": access_token, "token_type": "bearer"}
+
+@app.post("/create_user/", response_model=schemas.User)
+def create_user(
+    user: schemas.UserCreate = Depends(get_current_active_user), 
+    db: Session = Depends(get_db)
+    ):
     db_user = crud.get_user_by_email(db, email=user.email)
     if db_user:
         raise HTTPException(status_code=400, detail="Email already registered")
     return crud.create_user(db=db, user=user)
 
-
-@app.get("/users/", response_model=list[schemas.User])
+@app.get("/users/", response_model=List[schemas.User])
 def read_users(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
     users = crud.get_users(db, skip=skip, limit=limit)
     return users
-
 
 @app.get("/users/{user_id}", response_model=schemas.User)
 def read_user(user_id: int, db: Session = Depends(get_db)):
@@ -39,18 +105,21 @@ def read_user(user_id: int, db: Session = Depends(get_db)):
         raise HTTPException(status_code=404, detail="User not found")
     return db_user
 
-
-@app.post("/security_test_cases/", response_model=schemas.SecurityTestCase)
-def create_security_test_case(security_test_case: schemas.SecurityTestCaseCreate, db: Session = Depends(get_db)):
+@app.post("/create_security_test_case/", response_model=schemas.SecurityTestCase)
+def create_security_test_case(security_test_case: schemas.SecurityTestCaseBase, db: Session = Depends(get_db)):
     db_security_test_case = crud.get_security_test_cases_by_name(db, name=security_test_case.name)
     if db_security_test_case:
         raise HTTPException(status_code=400, detail="Name already registered")
     return crud.create_security_test_case(db=db, security_test_case=security_test_case)
 
-@app.get("/security_test_cases/", response_model=list[schemas.SecurityTestCase])
+@app.get("/security_test_cases/", response_model=List[schemas.SecurityTestCase])
 def read_security_test_cases(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
     security_test_cases = crud.get_security_test_cases(db, skip=skip, limit=limit)
     return security_test_cases
 
-
-
+@app.get("/security_test_cases/{security_test_case_id}", response_model=schemas.SecurityTestCase)
+def read_security_test_case(security_test_case_id: int, db: Session = Depends(get_db)):
+    db_security_test_case = crud.get_security_test_case(db, security_test_case_id=security_test_case_id)
+    if db_security_test_case is None:
+        raise HTTPException(status_code=404, detail="Security test case not found")
+    return db_security_test_case
--- backend/schemas.py
+++ backend/schemas.py
@@ -1,27 +1,35 @@
+from typing import Optional
 from pydantic import BaseModel
 
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+class TokenData(BaseModel):
+    username: Optional[str] = None
+
 class UserBase(BaseModel):
+    username: str
     email: str
+    full_name: Optional[str] = None
 
 
 class UserCreate(UserBase):
     password: str
 
-
 class User(UserBase):
     id: int
-    is_active: bool
+    hashed_password: str
+    disabled: Optional[bool] = None
+
 
     class Config:
         orm_mode = True
 
-
 class SecurityTestCaseBase(BaseModel):
     name: str
-    description: str = None
-
-class SecurityTestCaseCreate(SecurityTestCaseBase):
-    pass
+    description: Optional[str] = None
+    payload: Optional[str] = None
 
 class SecurityTestCase(SecurityTestCaseBase):
     id: int
